b82624715104fefaba006fdb3a8a753bf43744f4
lyzhib <74363315+lyzhib@users.noreply.github.com>
2024-05-01 21:35:01 +0500

    Update Settlements.jsx

--- client/src/pages/Settlements.jsx
+++ client/src/pages/Settlements.jsx
@@ -3,9 +3,26 @@ import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { styled } from '@mui/system';
+import {
+  TablePagination,
+  tablePaginationClasses as classes,
+} from '@mui/base/TablePagination';
 
 const Settlements = () => {
   const [settlements, setSettlements] = useState([]);
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   useEffect(() => {
     const fetchAllSettlements = async () => {
@@ -38,46 +55,147 @@ const Settlements = () => {
             Add new settlement
           </Link>
         </button>
-        <div className="links">
-          <Link className="link" to="/dormitories">
-            <h6>Dormitories</h6>
+        <button className="dormitory">
+          <Link to="/dormitories" style={{ color: "inherit", textDecoration: "none" }}>
+            Dormitories
           </Link>
-        </div>
-        <div className="links">
-          <Link className="link" to="/settlements">
-            <h6>Settlements</h6>
+        </button>
+        <button className="settlement">
+          <Link to="/settlements" style={{ color: "inherit", textDecoration: "none" }}>
+            Settlements
           </Link>
-        </div>
+        </button>
+        <button className="user">
+          <Link to="/users" style={{ color: "inherit", textDecoration: "none" }}>
+            Users
+          </Link>
+        </button>
       </div>
-       <table className="settlements">
+      <Root>
+       <table aria-label="custom pagination table">
         <thead>
          <tr>
-          <th scope="col">id</th>
-          <th scope="col">building</th>
-          <th scope="col">address</th>
+          <th>id</th>
+          <th>building</th>
+          <th>address</th>
          </tr>
         </thead>
         <tbody>
         {settlements && settlements.map((settlement) => (
-          <tr key={settlement.id} className="settlement">
-            <th scope="row">{settlement.id}</th>
+          <tr key={settlement.id}>
+            <td>{settlement.id}</td>
             <td>{settlement.building}</td>
             <td>{settlement.address}</td>
-            <button className="delete" onClick={() => handleDelete(settlement.id)}>Delete</button>
-            <button className="update">
-              <Link
-                to={`/update1/${settlement.id}`}
-                style={{ color: "inherit", textDecoration: "none" }}
-              >
-                Update
-              </Link>
-            </button>
+            <td>
+              <button className="delete" onClick={() => handleDelete(settlement.id)}>Delete</button>
+              <button className="update">
+                <Link
+                  to={`/update1/${settlement.id}`}
+                  style={{ color: "inherit", textDecoration: "none" }}
+                >
+                  Update
+                </Link>
+              </button>
+            </td>
           </tr>
           ))}
         </tbody>
+        <tfoot>
+          <tr>
+            <CustomTablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={13}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              slotProps={{
+                select: {
+                  'aria-label': 'Rows per page',
+                },
+                actions: {
+                  showFirstButton: true,
+                  showLastButton: true,
+                },
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </tr>
+        </tfoot>
        </table>
+       </Root>
     </div>
   );
 };
 
 export default Settlements;
+
+const grey = {
+  50: '#F3F6F9',
+  100: '#E5EAF2',
+  200: '#DAE2ED',
+  300: '#C7D0DD',
+  400: '#B0B8C4',
+  500: '#9DA8B7',
+  600: '#6B7A90',
+  700: '#434D5B',
+  800: '#303740',
+  900: '#1C2025',
+};
+
+const Root = styled('div')(
+  ({ theme }) => `
+  table {
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-size: 0.875rem;
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  td,
+  th {
+    border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[200]};
+    text-align: left;
+    padding: 8px;
+  }
+
+  th {
+    background-color: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+  }
+  `,
+);
+
+const CustomTablePagination = styled(TablePagination)`
+  & .${classes.toolbar} {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+
+    @media (min-width: 768px) {
+      flex-direction: row;
+      align-items: center;
+    }
+  }
+
+  & .${classes.selectLabel} {
+    margin: 0;
+  }
+
+  & .${classes.displayedRows} {
+    margin: 0;
+
+    @media (min-width: 768px) {
+      margin-left: auto;
+    }
+  }
+
+  & .${classes.spacer} {
+    display: none;
+  }
+
+  & .${classes.actions} {
+    display: flex;
+    gap: 0.25rem;
+  }
+`;
